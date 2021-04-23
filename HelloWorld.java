4e0a64792a24e4486f7f5023e8cf3eed3a87a7a5
anauroranon <58305348+anauroranon@users.noreply.github.com>
2021-04-23 17:02:57 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("A Kiril piace la banana lunga e matura");
+        System.out.println("Hello World!!!");
     }
 }
